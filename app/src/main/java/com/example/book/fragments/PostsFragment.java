2c43fa811f797700690957da8ee76c19622062ed
gandhinishi22 <gandhinishi22@gmail.com>
2022-04-18 16:56:24 -0400

    Add the filter of price

--- app/src/main/java/com/example/book/fragments/PostsFragment.java
+++ app/src/main/java/com/example/book/fragments/PostsFragment.java
@@ -18,9 +18,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 import com.example.book.EndlessRecyclerViewScrollListener;
+import com.example.book.MainActivity;
 import com.example.book.Post;
 import com.example.book.PostsAdapter;
 import com.example.book.R;
@@ -42,6 +47,7 @@ public class PostsFragment extends Fragment {
     private RecyclerView rvPosts;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
+    Spinner spinner;
     protected SwipeRefreshLayout swipeContainer;
     protected EndlessRecyclerViewScrollListener scrollListener;
     private LinearLayoutManager linearLayoutManager;
@@ -68,6 +74,10 @@ public class PostsFragment extends Fragment {
         allPosts = new ArrayList<>();
         adapter = new PostsAdapter(getContext(), allPosts);
 
+        //Spinner spinner = (Spinner)view.findViewById(R.id.action_money);
+
+        // Spinner click listener
+        //spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
         //Steps to use recycler view
         //0 create layout for one row in the list
         //1 create the adapter
@@ -104,14 +114,36 @@ public class PostsFragment extends Fragment {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         int id = item.getItemId();
+        Log.i(TAG,"Options selected");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search: {
+                Log.i(TAG, "SEARCH!");
+                Toast.makeText(getContext(), "Search!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.action_money: {
+                Log.i(TAG, "MONEY!");
+                clear();
+                queryPostsPriceHighLow();
+                Toast.makeText(getContext(), "High Low!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.action_lowhigh: {
+                Log.i(TAG, "Low High!");
+                clear();
+                queryPostsPriceLowHigh();
+                Toast.makeText(getContext(), "Low High!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            //return true;
         }
 
+        //return true;
         return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
@@ -120,7 +152,21 @@ public class PostsFragment extends Fragment {
         inflater.inflate(R.menu.menu_main, menu);
         //getMenuInflater().inflate(R.menu.menu_main, menu);
         final MenuItem searchItem = menu.findItem(R.id.action_search);
-        Log.i(TAG ,""+searchItem.isVisible());
+        final MenuItem spinnerItem = menu.findItem(R.id.action_money);
+        /*final Spinner spinner = (Spinner)MenuItemCompat.getActionView(spinnerItem);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i(TAG,"Spinner two");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.i(TAG,"Spinner");
+            }
+        });*/
+
+        Log.i(TAG ,""+spinnerItem.isVisible());
         final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -150,6 +196,53 @@ public class PostsFragment extends Fragment {
     }
 
 
+    protected void queryPostsPriceHighLow() {
+        Log.i(TAG,"Post: hello");
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        //get all the posts
+        query.include(Post.KEY_USER);
+
+        query.setLimit(20);
+        query.addDescendingOrder(Post.PRICE);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG,"Issue with getting posts",e);
+                }
+                for (Post post : posts) {
+                    Log.i(TAG,"Post: " + post.getBookTitle() + ", description: " + post.getBookDescription() +  ", category: " + post.getBookCategory() +", username: " + post.getUser().getUsername() +", price: " + Integer.toString(post.getPrice()) + " ISBN: " + post.getISBN());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    protected void queryPostsPriceLowHigh() {
+        Log.i(TAG,"Post: hello");
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        //get all the posts
+        query.include(Post.KEY_USER);
+
+        query.setLimit(20);
+        query.addAscendingOrder(Post.PRICE);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG,"Issue with getting posts",e);
+                }
+                for (Post post : posts) {
+                    Log.i(TAG,"Post: " + post.getBookTitle() + ", description: " + post.getBookDescription() +  ", category: " + post.getBookCategory() +", username: " + post.getUser().getUsername() +", price: " + Integer.toString(post.getPrice()) + " ISBN: " + post.getISBN());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
 
     //@Override
     protected void queryPostsWithTitle(String title) {
@@ -157,7 +250,10 @@ public class PostsFragment extends Fragment {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         //get all the posts
         query.include(Post.KEY_USER);
-        query.whereContains(Post.BOOK_TITLE, title);
+        if (Character.isDigit(title.charAt(0)))
+            query.whereContains(Post.BOOK_NUMBER,title);
+        else
+            query.whereContains(Post.BOOK_TITLE, title);
 
         query.setLimit(20);
         query.addDescendingOrder(Post.KEY_CREATED_KEY);
