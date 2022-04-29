70e5f3d6213566b6fcb5d957dcf49b5f9b182e1d
Ramiel-zibawi <96811553+Ramiel-zibawi@users.noreply.github.com>
2022-04-28 23:16:15 -0400

    Update MeetingConfirmation.java

--- app/src/main/java/com/example/book/MeetingConfirmation.java
+++ app/src/main/java/com/example/book/MeetingConfirmation.java
@@ -2,6 +2,8 @@ package com.example.book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,8 +29,8 @@ public class MeetingConfirmation extends AppCompatActivity {
     private ImageView sellerimage;
     private ImageView bookimage;
     private Button cancel;
-    private Button changeTime;
-    private Button changeLocation;
+    private Button edit;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +45,43 @@ public class MeetingConfirmation extends AppCompatActivity {
         buyerimage = findViewById(R.id.buyerimage);
         sellerimage = findViewById(R.id.sellerimage);
         bookimage = findViewById(R.id.bookimage);
-        cancel = findViewById(R.id.cancel);
-        changeTime = findViewById(R.id.changeTime);
-        changeLocation = findViewById(R.id.changeLocation);
+        button2 = findViewById(R.id.button2);
+        cancel = findViewById(R.id.button4);
+        edit = findViewById(R.id.edit);
 
         Transaction transaction = Parcels.unwrap(getIntent().getParcelableExtra("Transaction"));
         mc_username.setText(transaction.getSeller().getUsername());
         mc_username2.setText(transaction.getBuyer().getUsername());
         mc_location.setText(transaction.getLocation());
+        String longitude;
+        String latitude;
+        if(transaction.getLocation() == "Campus Center"){
+            longitude = "40.7431";
+            latitude = "-74.1784";
+        }
+        else if(transaction.getLocation() == "Library"){
+            longitude = "40.7437";
+            latitude = "-74.1781";
+        }
+        else if(transaction.getLocation() == "Gym"){
+            longitude = "40.7415";
+            latitude = "-74.1801";
+        }
+        else if(transaction.getLocation() == "Village Market"){
+            longitude = "40.7414";
+            latitude = "-74.1800";
+        }
+        else if(transaction.getLocation() == "Bookstore"){
+            longitude = "40.7412";
+            latitude = "-74.1781";
+        }
+        else{
+            longitude = "40.7412";
+            latitude = "-74.1781";
+
+        }
+
+
         mc_isbn.setText(String.valueOf(transaction.getPost().getBookTitle()));
         mc_price.setText(String.valueOf(transaction.getPost().getPrice()) + "$");
         String url = transaction.getPost().getImageUrl();
@@ -61,26 +92,34 @@ public class MeetingConfirmation extends AppCompatActivity {
         String url3 = transaction.getSeller().getParseFile("ProfilePic").getUrl();
         Glide.with(this).load(url3).circleCrop().into(sellerimage);
 
-        changeTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        changeLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                transaction.setCanceled(true);
+                Intent i = new Intent(MeetingConfirmation.this, MainActivity.class);
+                startActivity(i);
             }
         });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction.setCanceled(true);
+                Intent i = new Intent(MeetingConfirmation.this, ConfirmTransaction.class);
+                startActivity(i);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=" + longitude + "," + latitude);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
 
     }
 }
